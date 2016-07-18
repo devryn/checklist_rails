c1573d44d86b5712c24e491de514925feff295e2
Kate Walters <kawalters01@gmail.com>
2016-07-18 17:05:09 -0500

    Edit and delete buttons working

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,64 @@
 class ListsController < ApplicationController
+  def index
+    render template: 'lists/index.html.erb', locals: {
+      lists: List.all
+    }
+  end
+
+  def show
+    render locals: {
+      list: List.find(params[:id])
+    }
+  end
+
+  def new
+    render locals: {
+      list: List.new,
+    }
+  end
+
+  def create
+    list = List.new(list_params)
+    list.name = params[:list][:name]
+    if list.save
+      redirect_to list_path(list)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    render locals: {
+      list: List.find(params[:id])
+    }
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.body = params[:list][:body] if params[:list][:body].present?
+    if list.save
+      redirect_to list_path(list)
+    else
+      render locals: {
+        list:list
+      }
+    end
+  end
+
+  def destroy
+    if List.exists?(params[:id])
+
+      List.destroy(params[:id])
+      flash[:notice] = "List and tasks destroyed."
+      redirect_to "/lists"
+    else
+      flash[:alert] = "List was not deleted due to an error."
+    end
+  end
 
 
+  private
+  def list_params
+    params.require[:list].permit(:name)
+  end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,69 @@
 class TasksController < ApplicationController
+  def index
+    render template: 'tasks/index.html.erb', locals: {
+      tasks: Task.all
+    }
+  end
+
+  def show
+    render locals: {
+      task: Task.find(params[:id])
+    }
+  end
+
+  def new
+    render locals: {
+      task: Task.new,
+    }
+  end
+
+  def create
+    task = Task.new(task_params)
+    list.name = params[:list][:name]
+    task.position = params[:task][:position]
+    task.body = params[:task][:body]
+    task.completed = params[:task][:completed]
+    if task.save
+      redirect_to task_path(task)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    render locals: {
+      task: Task.find(params[:id])
+    }
+  end
+
+  def update
+    task = Task.find(params[:id])
+    list.name = params[:list][:name] if params[:list][:name].present?
+    task.position = params[:task][:position] if params[:task][:position].present?
+    task.body = params[:task][:body] if params[:task][:body].present?
+    task.completed = params[:task][:completed] if params[:task][:completed].present?
+    if task.save
+      redirect_to task_path(task)
+    else
+      render locals: {
+        task:task
+      }
+    end
+  end
+
+  def destroy
+    if Task.exists?(params[:id])
+
+      Task.destroy(params[:id])
+      flash[:notice] = "Task destroyed."
+      redirect_to "/tasks"
+    else
+      flash[:alert] = "Task was not deleted due to an error."
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:completed, :body, :position)
+  end
 end
